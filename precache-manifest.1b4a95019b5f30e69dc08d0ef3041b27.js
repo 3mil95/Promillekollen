75e6d6d3a36b7dbd2ab87468d972e64e7fe2d93b
Emil Clemedson <emilcle@gmail.com>
2019-12-29 15:31:59 +0100

    Updates

---
+++ precache-manifest.1b4a95019b5f30e69dc08d0ef3041b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8931c8f858f0541df9b08cd6619cb273",
+    "url": "/Promillekollen/index.html"
+  },
+  {
+    "revision": "54aa344267e333cc2ee8",
+    "url": "/Promillekollen/static/css/main.b028cb66.chunk.css"
+  },
+  {
+    "revision": "10b6e7112db0dca8bb0f",
+    "url": "/Promillekollen/static/js/2.7edca8e5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Promillekollen/static/js/2.7edca8e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54aa344267e333cc2ee8",
+    "url": "/Promillekollen/static/js/main.1428e1a7.chunk.js"
+  },
+  {
+    "revision": "5bf9e81b890dfef49c96",
+    "url": "/Promillekollen/static/js/runtime-main.8abb7469.js"
+  }
+]);
